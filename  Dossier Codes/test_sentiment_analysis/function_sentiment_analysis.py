cbe8fccb233aa449e74b6f328356c989b630d660
unknown <alyasltd@gmail.com>
2023-11-16 17:26:55 +0100

    sentiment_analysis_folder

---
+++  Dossier Codes/test_sentiment_analysis/function_sentiment_analysis.py
@@ -0,0 +1,56 @@
+import pandas as pd
+from pickle import FALSE
+import torch
+
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, AutoConfig
+import numpy as np
+from scipy.special import softmax
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+
+
+def preprocess(text):
+    new_text = []
+    for t in text.split(" "):
+        t = '@user' if t.startswith('@') and len(t) > 1 else t
+        t = 'http' if t.startswith('http') else t
+        new_text.append(t)
+    return " ".join(new_text)
+
+def analyze_sentiment_and_save(json_data, filename, dataframe=None):
+    # Load the model, tokenizer, and configuration
+    MODEL = "cardiffnlp/twitter-roberta-base-sentiment-latest"
+    tokenizer = AutoTokenizer.from_pretrained(MODEL)
+    config = AutoConfig.from_pretrained(MODEL)
+    model = AutoModelForSequenceClassification.from_pretrained(MODEL)
+
+    # Initialize DataFrame if it doesn't exist
+    columns = ["id", "positive", "neutral", "negative"]
+    try:
+        dataframe = pd.read_csv('sentiment_results.csv')
+    except FileNotFoundError:
+        dataframe = pd.DataFrame(columns=columns)
+
+    # Check if "text" key exists in json_data
+    if "text" in json_data:
+        # Apply preprocessing to the text
+        text = preprocess(json_data["text"])
+
+        # Tokenize and classify
+        encoded_input = tokenizer(text, return_tensors='pt')
+        output = model(**encoded_input)
+        scores = output.logits.detach().numpy()
+        scores = softmax(scores)
+
+        # Create a dictionary with the results
+        result = {"id": filename}
+        for i in range(scores.shape[1]):
+            l = config.id2label[i]
+            s = scores[0][i]
+            result[l] = np.round(float(s), 4)
+
+        # Append the results to the DataFrame
+        dataframe = pd.concat([dataframe, pd.DataFrame([result])], ignore_index=True)
+
+    return dataframe
+
---
+++  Dossier Codes/test_sentiment_analysis/sentiment_analysis.py
@@ -0,0 +1,64 @@
+from pickle import FALSE
+import torch
+
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, AutoConfig
+import numpy as np
+from scipy.special import softmax
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+
+# Preprocess text (username and link placeholders)
+def preprocess(text):
+    new_text = []
+    for t in text.split(" "):
+        t = '@user' if t.startswith('@') and len(t) > 1 else t
+        t = 'http' if t.startswith('http') else t
+        new_text.append(t)
+    return " ".join(new_text)
+
+# Your JSON data
+json_data = {
+    "id": "120328370310479873",
+    "creation": 1317523493.0,
+    "user_id": "230883057",
+    "social_network": "twitter",
+    "nsfw": False,
+    "request": ["d47670bd-00d9-4b4b-b654-af12857ee504"],
+    "metrics": {
+        "d47670bd-00d9-4b4b-b654-af12857ee504": {
+            "retweet_count": 0,
+            "reply_count": 0,
+            "like_count": 0,
+            "quote_count": 0
+        }
+    },
+    "text": "Cannabis Ingredient Helps Cancer Patients Regain Appetite and Taste http://t.co/IBpv7NKe via @herbalhealthsys #Cancer #MMJ #Cannabis #mmot",
+    "first_save": 1635509975.555947,
+    "hashtags": ["#CANCER", "#MMJ", "#CANNABIS", "#MMOT"]
+}
+
+# on load le model, le tokeniser et la configuration
+MODEL = "cardiffnlp/twitter-roberta-base-sentiment-latest"
+tokenizer = AutoTokenizer.from_pretrained(MODEL)
+config = AutoConfig.from_pretrained(MODEL)
+model = AutoModelForSequenceClassification.from_pretrained(MODEL)
+
+# on applique le pre traitement sur le text
+text = preprocess(json_data["text"])
+
+# Tokenize and classify
+encoded_input = tokenizer(text, return_tensors='pt')
+output = model(**encoded_input)
+scores = output.logits.detach().numpy()
+scores = softmax(scores)
+
+# Print labels and scores
+ranking = np.argsort(scores)
+ranking = ranking[::-1]
+for i in range(scores.shape[1]):
+    l = config.id2label[i]
+    s = scores[0][i]
+    print(f"{l}: {np.round(float(s), 4)}")
+
+
+
